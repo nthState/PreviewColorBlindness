f6dde1421454cfc6dcf72e3aae562a4c98de0a6b
Chris Davis <contact@nthstate.com>
2022-02-18 20:48:09 +0000

    Update License

--- Tests/PreviewColorBlindnessTests/PreviewColorBlindnessTests.swift
+++ Tests/PreviewColorBlindnessTests/PreviewColorBlindnessTests.swift
@@ -4,7 +4,7 @@
 //
 //  Copyright © 2022 Chris Davis, https://www.nthState.com
 //
-//  See https://github.com/nthState/PreviewColorBlindness/blob/master/LICENSE for license information.
+//  See https://github.com/nthState/PreviewColorBlindness/blob/main/LICENSE for license information.
 //
 
 import XCTest
