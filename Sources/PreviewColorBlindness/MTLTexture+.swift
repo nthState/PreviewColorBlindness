cdbed62cc3383eb0362906fbcf4ce62e6eb55621
Chris Davis <contact@nthstate.com>
2022-02-18 20:46:32 +0000

    Update cgImage code

--- Sources/PreviewColorBlindness/MTLTexture+.swift
+++ Sources/PreviewColorBlindness/MTLTexture+.swift
@@ -4,7 +4,7 @@
 //
 //  Copyright © 2022 Chris Davis, https://www.nthState.com
 //
-//  See https://github.com/nthState/PreviewColorBlindness/blob/master/LICENSE for license information.
+//  See https://github.com/nthState/PreviewColorBlindness/blob/main/LICENSE for license information.
 //
 
 import MetalKit
@@ -23,29 +23,27 @@ extension MTLTexture {
   
   var cgImage: CGImage? {
     
-    //assert(self.pixelFormat == .bgra8Unorm)
-    // read texture as byte array
     let rowBytes = self.width * 4
     let length = rowBytes * self.height
-    let bgraBytes = [UInt8](repeating: 0, count: length)
+    var bgraBytes = [UInt8](repeating: 0, count: length)
+    var bgraBuffer: vImage_Buffer?
     let region = MTLRegionMake2D(0, 0, self.width, self.height)
-    self.getBytes(UnsafeMutableRawPointer(mutating: bgraBytes), bytesPerRow: rowBytes, from: region, mipmapLevel: 0)
-    
-    // use Accelerate framework to convert from BGRA to RGBA
-    var bgraBuffer = vImage_Buffer(data: UnsafeMutableRawPointer(mutating: bgraBytes),
-                                   height: vImagePixelCount(self.height), width: vImagePixelCount(self.width), rowBytes: rowBytes)
-    let rgbaBytes = [UInt8](repeating: 0, count: length)
-    var rgbaBuffer = vImage_Buffer(data: UnsafeMutableRawPointer(mutating: rgbaBytes),
-                                   height: vImagePixelCount(self.height), width: vImagePixelCount(self.width), rowBytes: rowBytes)
+    bgraBytes.withUnsafeMutableBytes { ptr in
+      self.getBytes(ptr.baseAddress!, bytesPerRow: rowBytes, from: region, mipmapLevel: 0)
+      bgraBuffer = vImage_Buffer(data: ptr.baseAddress!,
+                                     height: vImagePixelCount(self.height), width: vImagePixelCount(self.width), rowBytes: rowBytes)
+    }
+
+    var rgbaBuffer: vImage_Buffer?
+    var rgbaBytes = [UInt8](repeating: 0, count: length)
+    rgbaBytes.withUnsafeMutableBytes { ptr in
+      rgbaBuffer = vImage_Buffer(data: ptr.baseAddress!,
+                                     height: vImagePixelCount(self.height), width: vImagePixelCount(self.width), rowBytes: rowBytes)
+    }
+
     let map: [UInt8] = [2, 1, 0, 3]
-    vImagePermuteChannels_ARGB8888(&bgraBuffer, &rgbaBuffer, map, 0)
-    
-    //        // flipping image vertically
-    //        let flippedBytes = bgraBytes // share the buffer
-    //        var flippedBuffer = vImage_Buffer(data: UnsafeMutableRawPointer(mutating: flippedBytes),
-    //                                          height: vImagePixelCount(self.height), width: vImagePixelCount(self.width), rowBytes: rowBytes)
-    //        vImageVerticalReflect_ARGB8888(&rgbaBuffer, &flippedBuffer, 0)
-    // create CGImage with RGBA
+    vImagePermuteChannels_ARGB8888(&bgraBuffer!, &rgbaBuffer!, map, 0)
+
     let colorScape = CGColorSpaceCreateDeviceRGB()
     let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
     guard let data = CFDataCreate(nil, rgbaBytes, length) else { return nil }
